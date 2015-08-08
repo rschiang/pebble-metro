2391fbb75d8ad528e7efe9f8ef81c773cb59b62b
Poren Chiang <ren.chiang@gmail.com>
2015-08-08 13:47:43 +0800

    Successfully wrote a watchface

--- src/metro.c
+++ src/metro.c
@@ -4,42 +4,65 @@ static Window *window;
 static TextLayer *text_layer;
 
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create(GRect(0, 55, 144, 50));
-  text_layer_set_background_color(text_layer, GColorClear);
-  text_layer_set_text_color(text_layer, GColorBlack);
-  text_layer_set_text(text_layer, "10:06");
-  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+    text_layer = text_layer_create(GRect(0, 55, 144, 50));
+    text_layer_set_background_color(text_layer, GColorClear);
+    text_layer_set_text_color(text_layer, GColorBlack);
+    text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
 
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+    layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+    text_layer_destroy(text_layer);
+}
+
+static void update_time(struct tm *tick_time) {
+    static char buffer[] = "00:00";
+
+    if (clock_is_24h_style() == true) {
+        strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    } else {
+        strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    }
+
+    text_layer_set_text(text_layer, buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+    update_time(tick_time);
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(window, animated);
+    window = window_create();
+    window_set_window_handlers(window, (WindowHandlers) {
+        .load = window_load,
+        .unload = window_unload,
+    });
+    const bool animated = true;
+    window_stack_push(window, animated);
+
+    // Register with system services
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+    // Get current time beforehand
+    time_t t = time(NULL);
+    struct tm *tick_time = localtime(&t);
+    update_time(tick_time);
 }
 
 static void deinit(void) {
-  window_destroy(window);
+    window_destroy(window);
 }
 
 int main(void) {
-  init();
+    init();
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
 
-  app_event_loop();
-  deinit();
+    app_event_loop();
+    deinit();
 }
